d80b8bf274aed4ab79c7e4c3e643c6aa00d990f5
aasupak <32544152+aasupak@users.noreply.github.com>
2019-05-12 21:39:40 -0500

    first project in course

---
+++ midterm1_python_dev236.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  7 20:35:08 2018
+
+@author: aasup
+"""
+
+input_test=input("enter some things eaten in the last 24 hours: ")
+
+x=("nuts" in input_test.lower())
+y=("seafood" in input_test.lower())
+z=("chocolate" in input_test.lower())
+
+print('It is', x,'that', input_test.lower(),'contains "nuts"')
+
+
+
+
+
+
+
+
+
+
+
+
